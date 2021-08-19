28cefddb98f5ddd95a864160a4b923cece79ddf4
mike smirnov <migel1976@gmail.com>
2021-08-19 17:05:59 +0300

    обработка данных полученных от сервера для отображения статей

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
-    Feed
-    {{apiUrl}}
-  </div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Something bad happened</div>
+    <div v-if="feed">
+      <div class="article-preview" v-for="(article,index) in feed.articles" :key="index">
+        <div class="article-meta">
+          <router-link 
+            :to="{name:'userProfile',params:{slug:article.author.username}}">
+            <img :src="article.author.image"/>
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name:'username',
+                params:{slug:article.author.username}
+                }"
+              class="author">
+            {{article.author.username}}
+            </router-link>
+            <span class="date">{{article.createdAt}}</span>
+          </div>
+          <div class="pull-xs-right">        
+            Add to Favorites
+
+          </div>
+        </div>
+          <router-link 
+            :to="{name:'userProfile',params:{slug:article.author.username}}">
+            <h1>{{article.title}}</h1>
+            <p>{{article.description}}</p>
+            <span>Read more...</span>
+            TAG LIST
+          </router-link>
+      </div>        
+    </div>        
+  </div>        
 </template>
 <script>
+  import {mapState} from 'vuex'
   import {actionTypes} from '@/store/modules/feed'
   export default{
     name:'Feed',
@@ -13,6 +46,13 @@
         type:String,
         requied:true
       }
+    },
+    computed:{
+      ...mapState({
+        isLoading: state=>state.feed.isLoading,
+        feed: state=>state.feed.data,
+        error: state=>state.feed.error
+      })
     },
     mounted(){
       this.$store.dispatch(actionTypes.getFeed,{apiUrl:this.apiUrl})
